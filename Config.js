e0e3a2ffd36b7683af879e22ef08a3da03578e5d
adminrt022gba-design <admin.rt022gba@gmail.com>
2026-08-26 15:52:06 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
-<script src="config.js"></script>
-<script src="app.js"></script>
+window.SUPABASE_URL = "https://arpshiebezebfnauegne.supabase.co/rest/v1/";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_u8GDumWrgWAB4Inf0JNZ3Q_BCXUlMoM";
