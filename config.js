53475c2c78faced6e6a4625c56325a7529222921
adminrt022gba-design <admin.rt022gba@gmail.com>
2026-08-26 16:06:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Koneksi Supabase Portal SIRT 022
 // JANGAN masukkan Secret Key / service_role di file ini.
 
-window.SUPABASE_URL = "https://arpshiebezebfnauegne.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://arpshiebezebfnauegne.supabase.co";
 window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_u8GDumWrgWAB4Inf0JNZ3Q_BCXUlMoM";
 
 // Jenis pengguna portal
